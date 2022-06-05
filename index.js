01ebeaaba05607362dfee26e067bbc56f5f6ed25
AdarshGoel2001 <86413540+AdarshGoel2001@users.noreply.github.com>
2022-06-05 16:53:48 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -5,21 +5,30 @@ const main = () => {
   const text = document.getElementById("4").value;
   const ratio = "0x" + document.getElementById("5").value;
   const hours = "0x" + document.getElementById("6").value;
-  const PapaHash = "0x" + document.getElementById("7").value;
+  const PapaHash = document.getElementById("7").value;
   const funness = document.getElementById("8").value;
   const random = "0x" + document.getElementById("9").value;
   const time = document.getElementById("10").value;
 
   var hashable = `{
     "number": "${day}",
+
     "WhatIDidToday": "${doings}",
+
     "SpecialThanksTo": "${mentions}",
+
     "PlansForTomorrow": "${text}",
+
     "RatioOfPlannedGoalsAchieved": "${ratio}",
+
     "approximateHoursDevoted": "${hours}",
+
     "parentHash": "${PapaHash}",
+
     "difficulty_satisfaction": "${funness}",
+
     "nonce": "${random}",
+
     "timestamp": "${time}",
   }`;
 
@@ -27,18 +36,29 @@ const main = () => {
 
   var obj = `>{
     number: ${day},
+
     WhatIDidToday: "${doings}",
+
     SpecialThanksTo: "${mentions}",
+
     PlansForTomorrow: "${text}",
+
     RatioOfPlannedGoalsAchieved: ${ratio},
+
     approximateHoursDevoted: ${hours},
-    hash: "0x" +"${hash}",
+
+    hash: "0x${hash}",
+
     parentHash: "${PapaHash}",
+
     difficulty_satisfaction: "${funness}",
+
     nonce: ${random},
+    
     timestamp: "${time}",
   }`;
   console.log(obj);
 
   document.getElementById("print").innerHTML = obj;
 };
+//https://github.com/AdarshGoel2001/My-twitter-blockchain-Diary-maker
